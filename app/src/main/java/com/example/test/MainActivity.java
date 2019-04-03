8e09f1e43fe57b10f7c3ddd32e0ad83ab33af485
arnor010 <47349704+arnor010@users.noreply.github.com>
2019-04-03 18:56:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.test;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,
+                    new HomeFragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_camera:
+                            selectedFragment = new CameraFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_memories:
+                            selectedFragment = new MemoriesFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
