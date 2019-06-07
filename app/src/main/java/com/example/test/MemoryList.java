1f19ff2db0eb04f22e40951b2eb64677d7ed2ff1
arnorietdijk <0964>
2019-06-07 10:05:28 +0200

    Newest version with memories almost done

---
+++ app/src/main/java/com/example/test/MemoryList.java
@@ -0,0 +1,325 @@
+package com.example.test;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MemoryList extends AppCompatActivity {
+    GridView gridView;
+    ArrayList<Food> list;
+    MemoryListAdapter adapter = null;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memory_list_activity);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+        list = new ArrayList<>();
+        adapter = new MemoryListAdapter(this, R.layout.memory_items, list);
+        gridView.setAdapter(adapter);
+
+        // get all data from sqlite
+        Cursor cursor = MemoriesFragment.sqLiteHelper.getData("SELECT * FROM FOOD");
+        list.clear();
+        if(cursor.getCount() > 0)
+        {
+            if(cursor.moveToFirst())
+            {
+                do
+                {
+                    try
+                    {
+                        int id = cursor.getInt(0);
+                        String title = cursor.getString(1);
+                        String description = cursor.getString(2);
+                        String loc = cursor.getString(3);
+                        byte[] image = cursor.getBlob(4);
+
+                        list.add(new Food(title, description, loc, image, id));
+                    }
+                    catch (IllegalStateException e)
+                    {
+                        //Do Nothing
+                    }
+                    catch (NullPointerException e)
+                    {
+                        //Do Nothing
+                    }
+                }
+                while(cursor.moveToNext());
+            }
+        }
+        cursor.close();
+        /*if (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String price = cursor.getString(2);
+            byte[] image = cursor.getBlob(3);
+
+            list.add(new Food(name, price, image, id));
+        }*/
+        adapter.notifyDataSetChanged();
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                CharSequence[] items = {"Update", "Delete"};
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MemoryList.this);
+
+                dialog.setTitle("Choose an action");
+                dialog.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (item == 0) {
+                            // update
+                            Cursor c = MemoriesFragment.sqLiteHelper.getData("SELECT id FROM FOOD");
+                            ArrayList<Integer> arrID = new ArrayList<Integer>();
+                            while (c.moveToNext()){
+                                arrID.add(c.getInt(0));
+                            }
+                            // show dialog update at here
+                            showDialogUpdate(MemoryList.this, arrID.get(position));
+
+                        } else {
+                            // delete
+                            Cursor c = MemoriesFragment.sqLiteHelper.getData("SELECT id FROM FOOD");
+                            ArrayList<Integer> arrID = new ArrayList<Integer>();
+                            while (c.moveToNext()){
+                                arrID.add(c.getInt(0));
+                            }
+                            showDialogDelete(arrID.get(position));
+                        }
+                    }
+                });
+                dialog.show();
+                return true;
+            }
+        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showPopup(view);
+                /*switch (position) {
+                    case position:
+
+                        break;
+                    default:
+                        Toast.makeText(MemoryList.this, "Wrong Input", Toast.LENGTH_LONG).show();
+                }*/
+            }
+        });
+    }
+
+    public void showPopup(View anchorView) {
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View popupView = inflater.inflate(R.layout.memory, null);
+
+        final PopupWindow popupWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        TextView txtTitle = (TextView) popupView.findViewById(R.id.txtTitle);
+        TextView txtDes = (TextView) popupView.findViewById(R.id.txtDes);
+        TextView txtLoc = (TextView) popupView.findViewById(R.id.txtLoc);
+        TextView txtDate = (TextView) popupView.findViewById(R.id.txtDate);
+        Button btnshare = (Button) popupView.findViewById(R.id.btnShare);
+
+        // If the PopupWindow should be focusable
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(false);
+
+        // If you need the PopupWindow to dismiss when when touched outside
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+
+        int location[] = new int[2];
+
+        // Get the View's(the one that was
+        // clicked in the Fragment) location
+        anchorView.getLocationOnScreen(location);
+
+        // Using location, the PopupWindow will be displayed right under anchorView
+        popupWindow.showAtLocation(anchorView, Gravity.CENTER,
+                0, 0);
+
+        View container = popupWindow.getContentView().getRootView();
+        if(container != null) {
+            WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams p = (WindowManager.LayoutParams)container.getLayoutParams();
+            p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+            p.dimAmount = 0.8f;
+            if(wm != null) {
+                wm.updateViewLayout(container, p);
+            }
+        }
+    }
+
+    ImageView imageViewFood;
+    private void showDialogUpdate(Activity activity, final int position){
+
+        final Dialog dialog = new Dialog(activity);
+        dialog.setContentView(R.layout.update_memory_activity);
+        dialog.setTitle("Update");
+
+        imageViewFood = (ImageView) dialog.findViewById(R.id.imageViewFood);
+        final EditText edtTitle = (EditText) dialog.findViewById(R.id.edtTitle);
+        final EditText edtDes = (EditText) dialog.findViewById(R.id.edtDes);
+        final EditText edtLoc = (EditText) dialog.findViewById(R.id.edtLoc);
+        Button btnUpdate = (Button) dialog.findViewById(R.id.btnUpdate);
+
+        // set width for dialog
+        int width = (int) (activity.getResources().getDisplayMetrics().widthPixels * 0.95);
+        // set height for dialog
+        int height = (int) (activity.getResources().getDisplayMetrics().heightPixels * 0.7);
+        dialog.getWindow().setLayout(width, height);
+        dialog.show();
+
+        imageViewFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // request photo library
+                ActivityCompat.requestPermissions(
+                        MemoryList.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        888
+                );
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    MemoriesFragment.sqLiteHelper.updateData(
+                            edtTitle.getText().toString().trim(),
+                            edtDes.getText().toString().trim(),
+                            edtLoc.getText().toString().trim(),
+                            MemoriesFragment.imageViewToByte(imageViewFood),
+                            position
+                    );
+                    dialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Update successfully!!!",Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception error) {
+                    Log.e("Update error", error.getMessage());
+                }
+                updateFoodList();
+            }
+        });
+    }
+
+    private void showDialogDelete(final int idFood){
+        final AlertDialog.Builder dialogDelete = new AlertDialog.Builder(MemoryList.this);
+
+        dialogDelete.setTitle("Warning!!");
+        dialogDelete.setMessage("Are you sure you want to this delete?");
+        dialogDelete.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    MemoriesFragment.sqLiteHelper.deleteData(idFood);
+                    Toast.makeText(getApplicationContext(), "Delete successfully!!!",Toast.LENGTH_SHORT).show();
+                } catch (Exception e){
+                    Log.e("error", e.getMessage());
+                }
+                updateFoodList();
+            }
+        });
+
+        dialogDelete.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialogDelete.show();
+    }
+
+    private void updateFoodList(){
+        // get all data from sqlite
+        Cursor cursor = MemoriesFragment.sqLiteHelper.getData("SELECT * FROM FOOD");
+        list.clear();
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String title = cursor.getString(1);
+            String des = cursor.getString(2);
+            String loc = cursor.getString(3);
+            byte[] image = cursor.getBlob(4);
+
+            list.add(new Food(title, des, loc, image, id));
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == 888){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 888);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "You don't have permission to access file location!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == 888 && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageViewFood.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
